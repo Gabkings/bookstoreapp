44122bfb10e4f7619245108787fff85881069caf
Gabriel Gitonga <gabworks51@gmail.com>
2020-01-09 00:07:43 +0300

    added authentications

--- books/views.py
+++ books/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
-from .models import Book
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView, DetailView
-# Create your views here.
 
-class BookListView(ListView):
+from .models import Book
+
+
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
-    context_object_name = 'book_list'
-    template_name= 'books/book_list.html'
-    
-class BookDetailView(DetailView):
+    context_object_name = "book_list"
+    template_name = "books/book_list.html"
+    login_url = "account_login"
+
+
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Book
-    context_object_name = 'book'
-    template_name = 'books/book_detail.html'    
+    context_object_name = "book"
+    template_name = "books/book_detail.html"
+    login_url = "account_login"
+    permission_required = "books.special_status"
+
--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -2,11 +2,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings 
+from django.conf.urls.static import static
 urlpatterns = [
     path('', include('pages.urls')),
     # User management
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
     path('books', include('books.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
